d9d5d6d4441e03b6a55cf96257d521e39522dbb5
StuoodJ <Jessebetea@gmail.com>
2024-07-02 21:38:13 -0400

    fix

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { range } from 'three/examples/jsm/nodes/Nodes.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -16,7 +17,7 @@ const stone = new THREE.TextureLoader().load('public/textures/stone.png');
 // define geometries
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const geometry2 = new THREE.BoxGeometry(1, 1, 1);
+
 const f = new THREE.CylinderGeometry(2, 2, 4, 32, 1, false, -90, Math.PI * 2);
 // define texture materials
 
@@ -24,21 +25,17 @@ const dirtM = new THREE.MeshBasicMaterial({map:dirt});
 const stoneM = new THREE.MeshBasicMaterial({map:stone});
 // define meshes
 
-const cube = new THREE.Mesh(geometry, dirtM);
-const cube2 = new THREE.Mesh(geometry2, stoneM);
-const cylinder = new THREE.Mesh(f, dirtM);
+const cube = new THREE.BoxGeometry(1, 1 , 1);
 
 // add meshes
 
 scene.add(cube);
-scene.add(cube2);
-scene.add(cylinder);
+
 // pre position
 
 camera.position.z = 5;
-cube2.position.x = 6;
 cube.position.x = -6;
-cylinder.rotation.x = 0.5;
+
 // animate
 
 function animate() {
